97d5ed7ecbed4c0aef213509d46e15e8747589c7
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-05-07 17:48:59 +0000

    ...

---
+++ visualobject/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var visualobjectF0C200FF3D824C55B070FCA9ADF125BF_DEBUG: IVisualPlugin;
+export default visualobjectF0C200FF3D824C55B070FCA9ADF125BF_DEBUG;
